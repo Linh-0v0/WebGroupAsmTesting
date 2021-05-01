253fb03dbf9e1df6e515f6059b226ad294203620
Vu Bui Khanh Linh <s3864120@rmit.edu.vn>
2021-05-01 15:41:44 +0700

    Update Login.js

--- Login.js
+++ Login.js
@@ -7,11 +7,10 @@ function check(){
         // alert("Login successfully")
         const emailvalue = document.getElementById("email").value;
         displayemail.setAttribute("value", "emailvalue");
-        location.pathname="my-account.html";
+        location.pathname="WebGroupAsmTesting/my-account.html";
         return false;
     } else {
         alert("Wrong password!");
-        location.reload();
     }
 }
 
