83ba65691f1fefb510e18d33e3b4745c2dc5e5ba
Vu Bui Khanh Linh <s3864120@rmit.edu.vn>
2021-05-01 15:31:01 +0700

    fix link

--- Login.js
+++ Login.js
@@ -1,10 +1,13 @@
 var email = document.getElementById("email");
 var password = document.getElementById("password");
-    
+var displayemail = document.getElementById("DisplayEmail");    
+
 function check(){
     if(password.value == "password"){
         // alert("Login successfully")
-        location.replace("http://linh-0v0.github.io/WebGroupAsmTesting/my-account.html");
+        const emailvalue = document.getElementById("email").value;
+        displayemail.setAttribute("value", "emailvalue");
+        window.location.href="http://linh-0v0.github.io/WebGroupAsmTesting/my-account.html";
         return false;
     } else {
         alert("Wrong password!");
@@ -13,8 +16,3 @@ function check(){
 }
 
 
-var displayemail = document.getElementById("DisplayEmail");
-function displayInputEmail(){
-    const emailvalue = document.getElementById("email").value;
-    displayemail.setAttribute("value", "emailvalue");
-}
