5150914ba9e025e4c7f62d03052e4eddfa007895
Vu Bui Khanh Linh <s3864120@rmit.edu.vn>
2021-05-02 19:02:19 +0700

    fix

--- Login.js
+++ Login.js
@@ -1,16 +1,22 @@
+var emailvalue = document.getElementById("email").value;
+var password = document.getElementById("password");
+var displayemail = document.getElementById("DisplayEmail");    
+var inputEmail;
+
+function showEmail(){
+    inputEmail = emailvalue;
+    displayemail.setAttribute("value", inputEmail);
+}
+
 function check(){
-    var password = document.getElementById("password");  
     if(password.value == "password"){
         // alert("Login successfully")
-        let Loginn = document.getElementsByClassName("btn").addEventListener("click", () => {
-            let inputEmail = document.getElementById("email").value;
-});
-        location.replace("my-account.html");
+        showEmail();
+        window.location.href = "my-account.html";
     } else {
         alert("Wrong password!");
     }
 }
-
 
 
 
