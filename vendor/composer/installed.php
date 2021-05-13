addfa66c1839b28d2a8d4b8812eff6985a82fec6
Vu Bui Khanh Linh <s3864120@rmit.edu.vn>
2021-05-14 03:26:06 +0700

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0f051fcb3f4709c79a27ce53878f7fb171d40388',
+    'name' => 'acme/acme-app',
+  ),
+  'versions' => 
+  array (
+    'acme/acme-app' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f051fcb3f4709c79a27ce53878f7fb171d40388',
+    ),
+  ),
+);
